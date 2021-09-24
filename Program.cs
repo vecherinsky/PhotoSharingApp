a22b6e888e996c661ee9a352d84c1b566aa75b65
Igor Vecherinsky <IVecherinsky@ibagroup.eu>
2021-09-24 17:31:07 +0300

    pull the project code

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Azure.Storage.Blobs;
+
+namespace PhotoSharingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            var connectionString = configuration.GetConnectionString("StorageAccount");
+            string containerName = "photos";
+
+            BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+            container.CreateIfNotExists();
+
+            string blobName = "docs-and-friends-selfie-stick";
+            string fileName = "docs-and-friends-selfie-stick.png";
+            BlobClient blobClient = container.GetBlobClient(blobName);
+            blobClient.Upload(fileName, true);
+
+            var blobs = container.GetBlobs();
+            foreach (var blob in blobs)
+            {
+                Console.WriteLine($"{blob.Name} --> Created On: {blob.Properties.CreatedOn:yyyy-MM-dd HH:mm:ss}  Size: {blob.Properties.ContentLength}");
+            }
+        }
+    }
+}
